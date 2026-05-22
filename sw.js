6424af7d3bff7302dac0cecd7307e3c4f153562f
waterslager <148389648+waterslager@users.noreply.github.com>
2026-05-22 23:00:52 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
+});
